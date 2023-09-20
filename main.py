bb163f2cf2613c29751c2bf5f0f924b8cc675c3f
Gatlin Farrington <72674932+gatlinfarrington@users.noreply.github.com>
2023-09-19 17:20:19 -0700

    setup

---
+++ main.py
@@ -0,0 +1,15 @@
+
+#Git Tutorial
+#This tutorial is setup for 4 individuals to see how git functions, please make sure you have gotten the instructions
+#The instructions can be gotten via a git request to the API at: http://gatlin-server:5001/instructions/<user#>
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+#Person 1 (add what instructions say) || Person 3 change after rebase
+
+
+
+#Person 2 (add what instructions say) || Person 4 change before rebase to cause a merge conflict
+
